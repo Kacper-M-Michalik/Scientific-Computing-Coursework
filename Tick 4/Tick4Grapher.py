1a770b80ac8ebc34bac0047f63c4e3226f891fdb
Kacper Michalik <km924@cam.ac.uk>
2024-01-08 22:39:08 +0000

    Updated Grapher

--- Tick 4/Tick4Grapher.py
+++ Tick 4/Tick4Grapher.py
@@ -7,33 +7,37 @@ F = open('SimData.data', 'rb')
 F.close()
 
 MeanGinis = np.empty(len(CapitalGainsMults))
+VarianceGinis = np.empty(len(CapitalGainsMults))
 MeanMobilities = np.empty(len(CapitalGainsMults))
+VarianceMobilities = np.empty(len(CapitalGainsMults))
 
 for (Mult, AllGinis, AllRunMobilities) in OutputList:
-    MobilityTotal = 0
-    for MobilityForRun in AllRunMobilities:        
-        #MobilityTotal += np.max(MobilityForRun)
-        MobilityTotal += np.mean(MobilityForRun[MobilityMeanFromDay:])
+    
+    WantedMobilities = np.empty(Reruns)
+    for i, MobilityForRun in enumerate(AllRunMobilities):  
+        WantedMobilities[i] = MobilityForRun[MobilityMeanFromDay - 1]
 
     i = CapitalGainsMults.index(Mult)
     MeanGinis[i] = np.mean(AllGinis)
-    MeanMobilities[i] = MobilityTotal / Reruns
+    VarianceGinis[i] = np.std(AllGinis) / np.sqrt(len(AllGinis))
+    MeanMobilities[i] = np.mean(WantedMobilities)
+    VarianceMobilities[i] = np.std(WantedMobilities) / np.sqrt(len(WantedMobilities))
     
 Fig, Axes = plt.subplots(figsize=(8, 6))
 Axes.set_title("Impact of capital gains rate on inequality and mobility")
 Axes.set_xlabel("Gini coefficient")
 Axes.set_ylabel("Mobility after {fromVal} steps".format(fromVal=MobilityMeanFromDay))  
 
-#Axes.errorbar(GiniMeans, MobilityMeans, MobilityVariabilties, GiniVariabilties, color = (1, 0, 0), zorder = 0)
+Axes.errorbar(MeanGinis, MeanMobilities, VarianceMobilities, VarianceGinis, color = (1, 0, 0), zorder = 0)
 Axes.plot(MeanGinis, MeanMobilities, zorder = 1)
 Axes.scatter(MeanGinis, MeanMobilities, color = (0, 0.75, 0.75), zorder = 2)
 
 if (len(MeanGinis) > 0):
-    OffsetX = MeanGinis[0] * 0.0005
+    OffsetX = MeanGinis[0] * 0.00055
     OffsetY = - MeanMobilities[0] * 0.018
     for i, (x, y) in enumerate(zip(MeanGinis, MeanMobilities)):
-        plt.text(x + OffsetX, y + OffsetY, CapitalGainsMults[i])
+        plt.text(x + OffsetX, y + OffsetY, CapitalGainsMults[i], fontsize = 9)
 
-plt.text(0.04,0.88, "N = {nVal}\nTimesteps = {tVal}\nRuns per CGR = {runVal}".format(nVal=N, tVal=T, runVal=Reruns), fontsize = 8, color = 'r', transform=Axes.transAxes)
+plt.text(0.04,0.88, "N = {nVal}\nTimesteps = {tVal}\nRuns per CGR = {runVal}".format(nVal=N, tVal=T, runVal=Reruns), fontsize = 7, color = 'r', transform=Axes.transAxes)
 
 plt.show()
